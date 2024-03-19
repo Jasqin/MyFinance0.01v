b69ad1d2b1870a6b8a600997307af2e9660f52e9
Jasqin <110784990+Jasqin@users.noreply.github.com>
2024-03-19 02:06:48 +0100

    0.02v

--- MyFin/MainWindow.xaml.cs
+++ MyFin/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MyFin
 {
@@ -20,6 +22,10 @@ namespace MyFin
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly SqlConnection Connect = new SqlConnection(@"Data Source=The_chosen_one\SQLEXPRESS;Initial Catalog=MyFinance;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+        
+
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -47,12 +53,58 @@ namespace MyFin
 
         private void RegButt_Click(object sender, RoutedEventArgs e)
         {
+            if (NUserName.Text is null || NSecName.Text is null || NPass.Text is null || NName.Text is null)
+            {
+                MessageBox.Show("Fill all fields!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (Connect.State != ConnectionState.Open)
+                {
+                    try
+                    {
+                        Connect.Open();
 
-        }
+                        string countUsernames = "select count(UserId) from Users where Username = @user";
 
-        private void LogPassword_TextChanged(object sender, TextChangedEventArgs e)
-        {
+                        using(SqlCommand tryUser = new SqlCommand(countUsernames, Connect))
+                        {
+                            tryUser.Parameters.AddWithValue("@user", NName.Text.Trim());
+                            int count = (int)(tryUser.ExecuteScalar());
 
+                            if (count > 0)
+                            {
+                                MessageBox.Show(NName.Text.Trim() + " is taken", "nope", MessageBoxButton.OK, MessageBoxImage.Stop);
+                            } else
+                            {
+                                string insertData = "INSERT INTO Users (Name, SecondName, Username, Password)\r\nVALUES (@Name, @SName, @UName, @Pass);";
+
+                                using (SqlCommand Inse = new SqlCommand(insertData, Connect))
+                                {
+                                    Inse.Parameters.AddWithValue("@Name", NName.Text.Trim());
+                                    Inse.Parameters.AddWithValue("@SName", NSecName.Text.Trim());
+                                    Inse.Parameters.AddWithValue("@UName", NUserName.Text.Trim());
+                                    Inse.Parameters.AddWithValue("@Pass", NPass.Text.Trim());
+
+                                    Inse.ExecuteNonQuery();
+
+                                    MessageBox.Show("All done!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                                }
+                            }
+                        }
+
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error! " + ex, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    finally
+                    {
+                        Connect.Close();
+                    }
+                }
+            }
         }
 
         private void LogUserName_TextChanged(object sender, TextChangedEventArgs e)
@@ -62,7 +114,51 @@ namespace MyFin
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            if(LogUserName.Text is null || LogUserName.Text is null || LogPassword.Text is null || LogPassword.Text is null)
+            {
+                MessageBox.Show("Fill all fields!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if(Connect.State != ConnectionState.Open)
+                {
+                    try
+                    {
+                        Connect.Open();
+                        string logindata = "select * from users where Username = @username and Password = @password;";
 
+                        using (SqlCommand Log = new SqlCommand(logindata, Connect))
+                        {
+                            Log.Parameters.AddWithValue("@username", LogUserName.Text.Trim());
+                            Log.Parameters.AddWithValue("@password", LogPassword.Text.Trim());
+                            Log.ExecuteNonQuery();
+
+                            SqlDataAdapter Adap = new SqlDataAdapter(Log);
+                            DataTable table = new DataTable();
+                            Adap.Fill(table);
+                            if (table.Rows.Count == 1)
+                            {
+
+                                MessageBox.Show("Login successfully!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Inncorect logins!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
+
+                        }
+
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("Error! " + ex , "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    finally 
+                    {
+                        Connect.Close();
+                    }
+                }
+            }
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
